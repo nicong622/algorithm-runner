7581633ab3200dcd52b49d32f0bb139e13d8ad0a
nicong <liangningcong@gmail.com>
2021-12-15 17:20:58 +0800

    添加希尔排序

---
+++ problems/shell_sort/index.test.js
@@ -0,0 +1,7 @@
+const shellSort = require('./index')
+
+const input = [4,3,5,9,7,1,2,0,10]
+
+test(JSON.stringify(input), () => {
+  expect(shellSort(input)).toEqual(expect.arrayContaining([0,1,2,3,4,5,7,9,10]))
+})
