f8c9d7be047c90d2241841af938bd5bbf0aab645
nicong <liangningcong@gmail.com>
2021-12-28 11:57:06 +0800

    update

--- problems/back_tracking/parenthesis.js
+++ problems/back_tracking/parenthesis.js
@@ -8,26 +8,26 @@
 // 输入：n = 1
 // 输出：["()"]
 
-const generateParenthesis = function (n) {
+function generateParenthesis(n) {
   const res = [];
   const backTracing = (lRemain, rRemain, str) => {
     // 左右括号所剩的数量，str是当前构建的字符串
-    if (str.length == 2 * n) {
+    if (str.length === 2 * n) {
       // 字符串构建完成
       res.push(str); // 加入解集
       return; // 结束当前递归分支
     }
     if (lRemain > 0) {
       // 只要左括号有剩，就可以选它，然后继续做选择（递归）
-      backTracing(lRemain - 1, rRemain, `${str }(`);
+      backTracing(lRemain - 1, rRemain, `${str}(`);
     }
     if (lRemain < rRemain) {
       // 右括号比左括号剩的多，才能选右括号
-      backTracing(lRemain, rRemain - 1, `${str })`); // 然后继续做选择（递归）
+      backTracing(lRemain, rRemain - 1, `${str})`); // 然后继续做选择（递归）
     }
   };
   backTracing(n, n, ''); // 递归的入口，剩余数量都是n，初始字符串是空串
   return res;
-};
+}
 
 console.log(generateParenthesis(3));
---
+++ problems/back_tracking/permute.test.ts
@@ -0,0 +1,7 @@
+import permute from './permute';
+
+console.log(permute([1, 2, 3]));
+
+test('permute', () => {
+  expect(1).toEqual(1);
+});
